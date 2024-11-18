dbdaf3461d70aff2183897b6122f3286389a43dd
Yiannis <yiannis.leblanc@icloud.com>
2024-11-18 01:13:33 +0100

    Création de la fonction randomMoves dans moves.h

--- moves.c
+++ moves.c
@@ -154,3 +154,45 @@ void updateLocalisation(t_localisation *p_loc, t_move m)
     *p_loc = move(*p_loc, m);
     return;
 }
+
+void randomMoves(int* probs, t_move* array) {
+    const int numVal = 9;
+    int randVal;
+    const int totalCoef = probs[F_10] + probs[F_20] + probs[F_30] + probs[B_10] + probs[T_LEFT] + probs[T_RIGHT] + probs[U_TURN] - 1;
+
+
+    srand(time(NULL));
+    for (int i = 0; i <= numVal; i++) {
+        randVal = (rand()%(totalCoef))+1;
+        if (randVal <= probs[F_10]) {
+            array[i] = F_10;
+            probs[F_10]--;
+        } else if (randVal <= probs[F_20]) {
+            array[i] = F_20;
+            probs[F_20]--;
+        }
+        else if (randVal <= probs[F_30]) {
+            array[i] = F_30;
+            probs[F_30]--;
+        }
+        else if (randVal <= probs[B_10]) {
+            array[i] = B_10;
+            probs[B_10]--;
+        }
+        else if (randVal <= probs[T_LEFT]) {
+            array[i] = T_LEFT;
+            probs[T_LEFT]--;
+        }
+        else if (randVal <= probs[T_RIGHT]) {
+            array[i] = T_RIGHT;
+            probs[T_RIGHT]--;
+        }
+        else if (randVal <= probs[U_TURN]) {
+            array[i] = U_TURN;
+            probs[U_TURN]--;
+        }
+        else {
+            array[i] = NONE;
+        }
+    }
+}
