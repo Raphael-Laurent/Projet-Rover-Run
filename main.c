719a5f194cfd73fe60f35cb30ee14d73306476a7
HugoWiltEFREI <hugo.wilt@efrei.net>
2024-11-27 00:16:25 +0100

    Update

--- main.c
+++ main.c
@@ -20,7 +20,7 @@ int main() {
     int total_min_value = 0;
     int base_found = 0;
 
-    while (total_min_value < 100 && !base_found) {
+    while (total_min_value < 50 && !base_found) {
         t_tree mytree = getTree(&map, rover);
 
         printf("\nGenerated Tree:\n");
@@ -29,7 +29,10 @@ int main() {
         t_node *min = minLocalisation(mytree.root, NULL, map);
 
         if (map.costs[min->local.pos.y][min->local.pos.x] == 0) {
-            printf("\nBase found at (%d, %d) with a fuel usage of : %d !\n", min->local.pos.x, min->local.pos.y,min->value);
+            if (total_min_value == 0){
+                total_min_value = min->value;
+            }
+            printf("\nBase found at (%d, %d) with a fuel usage of : %d !\n", min->local.pos.x, min->local.pos.y,total_min_value);
             base_found = 1;
         } else {
             total_min_value += min->value;
